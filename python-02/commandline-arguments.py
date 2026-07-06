1f6deb5df9a0205fe157a4f284f841ca15e8673a
galib-ds <contactmegalib@gmail.com>
2026-07-06 23:41:27 +0600

    Docs: Learned pythone commandline arguments, Data type and math functions

---
+++ python-02/commandline-arguments.py
@@ -0,0 +1,8 @@
+#argv sys argument
+import sys
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+z = x+y
+print("x=",x,"y=",y,"z=",z)
---
+++ python-02/mathematical-functions.py
@@ -0,0 +1,85 @@
+print("##############################")
+# abs() function
+print("abs(-45) :", abs(-45))
+print("abs(100.12) :", abs(100.12))
+
+print("##############################")
+
+#max() function
+print("max(80, 100, 1000) :",max(80, 100, 1000))
+print("max(-20, 100, 400) :",max(-20, 100, 400))
+print("max(-80, -20, -10) :",max(-80, -20, -10))
+print("max(0, 100, -400) :",max(0, 100, -400))
+
+print("##############################")
+
+#min() function
+print("min(80, 100, 1000) :",min(80, 100, 1000))
+print("min(-20, 100, 400) :",min(-20, 100, 400))
+print("min(-80, -20, -10) :",min(-80, -20, -10))
+print("min(0, 100, -400) :",min(0, 100, -400))
+
+print("##############################")
+
+#round() function
+print("round(70.23456) :", round(70.23456))
+print("round(56.659,1) :", round(56.659,1))
+print("round(80.264,2) :", round(80.264,2))
+print("round(100.000056,3) :", round(100.000056,3))
+print("round(-100.000056,3) :", round(-100.000056,3))
+
+print("##############################")
+
+#ceil() function
+import math
+print("math.ceil(-45.17) :", math.ceil(-45.17))
+print("math.ceil(100.12) :", math.ceil(100.12))
+print("math.ceil(100.72) :", math.ceil(100.72))
+print("math.celi(math.pi) :", math.ceil(math.pi))
+
+print("##############################")
+
+#floor() function
+print("math.floor(-45.17) :", math.floor(-45.17))
+print("math.floor(100.12) :", math.floor(100.12))
+print("math.floor(100.72) :", math.floor(100.72))
+print("math.floor(math.pi) :", math.floor(math.pi))
+
+print("##############################")
+
+#exp() functiom
+print("math.exp(-45.17) :", math.exp(-45.17))
+print("math.exp(100.12) :", math.exp(100.12))
+print("math.exp(100.72) :", math.exp(100.72))
+print("math.exp(math.pi) :", math.exp(math.pi))
+
+print("##############################")
+
+#log() function
+print("math.log(100.12) :", math.log(100.12))
+print("math.log(100.72) :", math.log(100.72))
+print("math.log(math.pi) :", math.log(math.pi))
+
+print("##############################")
+
+#log10() function
+print("math.log10(100.12) :", math.log10(100.12))
+print("math.log10(100.72) :", math.log10(100.72))
+print("math.log10(math.pi) :", math.log10(math.pi))
+
+print("##############################")
+
+#pow() function
+print("math.pow(100,2) :", math.pow(100,2))
+print("math.pow(100,-2) :", math.pow(100,-2))
+print("math.pow(2,4) :", math.pow(2,4))
+print("math.pow(3,0) :", math.pow(3,0))
+
+print("##############################")
+
+#sqrt() function
+print("math.sqrt(100) :", math.sqrt(100))
+print("math.sqrt(7) :", math.sqrt(7))
+print("math.sqrt(math.pi) :", math.sqrt(math.pi))
+
+print("##############################")
