835f15a68c3a07b8da24d7fd25c67aa24ffe1ce3
galib-ds <contactmegalib@gmail.com>
2026-07-09 02:13:27 +0600

    Docs: Mathemetical constants in python

---
+++ python-03/mathemetical-constants.py
@@ -0,0 +1,10 @@
+import math
+print("############################")
+# constant pi
+pi = math.pi
+print("pi :", pi)
+
+# constant e
+e = math.e
+print("e :", e)
+print("############################")
