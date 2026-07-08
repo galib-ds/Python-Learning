53e5e28c668795d2274ae3695250f50fd63a4007
galib-ds <contactmegalib@gmail.com>
2026-07-09 02:28:56 +0600

    Docs: trigonometric inverce function

---
+++ python-03/trigonometric-inverce-function.py
@@ -0,0 +1,29 @@
+import math
+
+print("#############################")
+# asin(x) function
+print("asin(0.64) :", math.asin(0.64))
+print("asin(0) :", math.asin(0))
+print("asin(-1) :", math.asin(-1))
+print("asin(1) :", math.asin(1))
+print("#############################")
+# acos(x) function
+print("acos(0.64) :", math.acos(0.64))
+print("acos(0) :", math.acos(0))
+print("acos(-1) :", math.acos(-1))
+print("acos(1) :", math.acos(1))
+print("#############################")
+# atan(x) function
+print("atan(0.64) :", math.atan(0.64))
+print("atan(0) :", math.atan(0))
+print("atan(10) :", math.atan(10))
+print("atan(-1) :", math.atan(-1))
+print("atan(1) :", math.atan(1))
+print("#############################")
+# atan2(x,y) function
+print("atan2(-0.5,-0.5) :", math.atan2(-0.5,-0.5))
+print("atan2(0.5,0.5) :", math.atan2(0.5,0.5))
+print("atan2(5,5) :", math.atan2(5,5))
+print("atan2(-10,10) :", math.atan2(-10,10))
+print("atan2(10,20) :", math.atan2(10,20))
+print("#############################")
