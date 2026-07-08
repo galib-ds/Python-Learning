52222b5bd955d137708bbe6e5f91dfcb71fc8c0e
galib-ds <contactmegalib@gmail.com>
2026-07-09 02:08:34 +0600

    Docs: Trigonometric functions in python

---
+++ python-03/trigonometric-function.py
@@ -0,0 +1,28 @@
+import math
+
+#Arguments in the trigonometric function are in radien unit
+print("#########################################")
+#sin() function
+print("sine(3) :", math.sin(3))
+print("sin(-3) :", math.sin(-3))
+print("sin(0) :", math.sin(0))
+print("sin(math.pi) :", math.sin(math.pi))
+print("sin(math.pi/2) :", math.sin(math.pi/2))
+
+print("#########################################")
+#cos() function
+print("cos(3) :", math.cos(3))
+print("cos(-3) :", math.cos(-3))
+print("cos(0) :", math.cos(0))
+print("cos(math.pi) :", math.cos(math.pi))
+print("cos(2*math.pi) :", math.cos(2*math.pi))
+
+print("#########################################")
+#tan() function
+print("tan(3) :", math.tan(3))
+print("tan(-3) :", math.tan(-3))
+print("tan(0) :", math.tan(0))
+print("tan(math.pi/2) :", math.tan(math.pi/2))
+print("tan(math.pi/4) :", math.tan(math.pi/4))
+
+print("#########################################")
