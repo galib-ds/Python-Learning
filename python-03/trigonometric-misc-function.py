57511dd4cdc737a9fa72167f877ec06c4ae5b353
galib-ds <contactmegalib@gmail.com>
2026-07-09 02:42:52 +0600

    Docs: Trigonometrics Misc functions

---
+++ python-03/trigonometric-misc-function.py
@@ -0,0 +1,23 @@
+import math
+print("#############################")
+
+#hypot(x,y) canculates x*x + y*y
+print("hypot(10,15) :", math.hypot(10,15))
+print("hypot(15,10) :", math.hypot(15,10))
+print("hypot(-10,15) :", math.hypot(-10,15))
+print("hypot(10,-15) :", math.hypot(10,-15))
+print("#############################")
+
+#degrees(x)
+print("degrees(0) :", math.degrees(0))
+print("degrees(math.pi) :", math.degrees(math.pi))
+print("degrees(math.pi/2) :", math.degrees(math.pi/2))
+print("degrees(math.pi/4) :", math.degrees(math.pi/4))
+print("#############################")
+
+#radians(x)
+print("radians(0) :", math.radians(0))
+print("radians(180) :", math.radians(180))
+print("radians(90) :", math.radians(90))
+print("radians(45) :", math.radians(45))
+print("#############################")
