f66734d640992459518a6e542ffb5e510039ea78
galib-ds <contactmegalib@gmail.com>
2026-07-10 16:48:42 +0600

    Docs: Arithmetic operators in python

---
+++ python-04/arithmetic-operators.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+c = 0
+print('a=',a,'b=',b)
+c = a+b
+print("addition value of c is = ", c)
+c = a-b
+print("subtraction value of c is = ",c)
+c = a*b
+print("multiplication value of c is = ",c)
+c = a/b
+print("division value of c is = ",c)
+c = a%b
+print("mod value of c is = ",c)
+c = a**b
+print("exponent value of c is = ",c)
+c = a//b
+print("floor value of c is = ",c)
---
+++ python-04/input-output.py
@@ -0,0 +1,3 @@
+n = input("Enter your name: ")
+print("My name is",n)
+print(f'My name is {n}')
