5640d1f359db68e261b17eaba269c2d8c923610e
galib-ds <contactmegalib@gmail.com>
2026-07-11 00:39:17 +0600

    Docs: different operation inside print function

---
+++ python-04/string-12.py
@@ -0,0 +1,7 @@
+# string methodes
+print("gOOd mOrNiNg tO aLL".lower())
+print("gOOd mOrNiNg tO aLL".upper())
+print("gOOd mOrNiNg tO aLL".find('tO'))
+print("gOOd mOrNiNg tO aLL".find('to'))
+print("gOOd mOrNiNg tO aLL".replace('aLL','everybody'))
+print("gOOd mOrNiNg tO aLL".replace('all','everybody'))
