8050b061a71186ba06a64d3796ea1400f62455ab
galib-ds <contactmegalib@gmail.com>
2026-07-11 00:32:21 +0600

    Docs: ude of formate() operation in print()

---
+++ python-04/string-11.py
@@ -0,0 +1,8 @@
+# formating numbers
+print("Required binary representation of {0} is {0:b}".format(20))
+
+# formating floats
+print("Exponent representation: {0:e}".format(1566.345))
+
+# round off
+print("One third is: {0:.3f}".format(1/3))
