ae481b6c1da1165c66504888fa9a95dbf6298862
galib-ds <contactmegalib@gmail.com>
2026-07-10 20:34:21 +0600

    Docs: Operator precidence

---
+++ python-04/operator-precedence.py
@@ -0,0 +1,13 @@
+# **                           Exponentiation
+# ~ +@ -@                      Complement, unary plus and minus
+# * / % //                     Multiply, Divide, Mode, Floor division
+# + -                          Addition and Subtraction
+# >> <<                        Right and Left Bitwise shift
+# &                            Bitwise operators AND
+# ^ |                          Bitwise operators EXOR and OR
+# <= < > >=                    Comparison operators
+# <> == !=                     Equality operators
+# = %= /= //= -= += *= **=     Assignment operators              
+# is  is not                   Identity operators
+# in not in                    Membership operators
+# not or and                   Logical operators
