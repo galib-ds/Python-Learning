fb690fbdfa4c90b8f074d875c80c0caf4f514aa0
galib-ds <contactmegalib@gmail.com>
2026-07-11 11:20:50 +0600

    Docs: string operators in python

---
+++ python-05/string-operation.py
@@ -0,0 +1,14 @@
+str1 = 'Hello '
+str2 = 'Python'
+
+print("str1 + str2 =", str1 + str2)
+print("str1 * 3 =", str1 * 3)
+print("str1[4] =", str1[4])
+print("str1[1:3] =", str1[1:3])
+print("str1[1:] =", str1[1:])
+print("'H' in str1 =",'H' in str1)
+print("'h' in str1 =", 'h' in str1)
+print("'c' in str1 =", 'c' in str1)
+print("'H' not in str1 =", 'H' not in str1)
+print("'h' not in str1 =", 'h' not in str1)
+print("'c' not in str1 =", 'c' not in str1)
