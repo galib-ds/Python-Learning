081bbf2762367a384aaa56d5fe30cc701da9f25d
galib-ds <contactmegalib@gmail.com>
2026-07-11 11:31:48 +0600

    Docs: string formating operators in python

---
+++ python-05/string-formating-opreators.py
@@ -0,0 +1,13 @@
+# using string formating operators
+
+name = 'Galib'
+print("wellcome %s" %name)
+age = 25
+print("age = %d" %age)
+print("age = %s" %age)
+print("age = %o" %age)
+print("age = %x" %age)
+print("age = %e" %age)
+print("age = %.2e" %age)
+print("age = %f" %age)
+print("age = %.2f" %age)
