2130c6a6c3a3c9500e9e36b435913b05c95847db
galib-ds <contactmegalib@gmail.com>
2026-07-11 12:27:53 +0600

    Docs: lists in python

---
+++ python-05/list.py
@@ -0,0 +1,10 @@
+# lista are like array but the only diifference is
+# it can be of any types
+
+list1 =['physics','chemistry',2005,2026]
+list2 = [1,-2, 3+4j, 4, 5.55]
+list3 = ["a","b",'c',"d"]
+
+print(list1)
+print(list2)
+print(list3)
